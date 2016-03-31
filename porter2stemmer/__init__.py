9494cdf7ec8b272a73fee9cf4187a23d1196e783
Evan Dempsey <evandempsey@gmail.com>
2016-03-31 20:51:22 +0100

    Fixing flake8 warnings.

--- porter2stemmer/__init__.py
+++ porter2stemmer/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
+from .porter2stemmer import Porter2Stemmer  # flake8: noqa
 
 __author__ = 'Evan Dempsey'
 __email__ = 'me@evandempsey.io'
 __version__ = '1.0'
-
-from .porter2stemmer import Porter2Stemmer
